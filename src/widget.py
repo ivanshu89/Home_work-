35992a8156cd1cb37390c4f88b50111839b2a241
ivanshu <ivanshu899@gmail.com>
2024-07-16 10:46:31 +0300

    get_date added

--- src/widget.py
+++ src/widget.py
@@ -5,3 +5,7 @@ def mask_account_card(card_number: str) -> str:
         return f"{card_number[:-20]}** {card_number[-4:]}"
     else:
         return f"{card_number[:-16]}{card_number[-16: -12]} {card_number[-12: -10]}** **** {card_number[-4:]}"
+
+
+def get_date(long_date: str) -> str:
+    return f"{long_date[8:10]}.{long_date[5:7]}.{long_date[:4]}"
