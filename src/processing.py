a459e0c43022a42587f8620374282078578a076d
ivanshu <ivanshu899@gmail.com>
2024-10-31 10:41:17 +0300

    type anatations add

--- src/processing.py
+++ src/processing.py
@@ -1,7 +1,11 @@
-def filter_by_state(data, state='EXECUTED'):
+from typing import List, Dict, Any
+
+
+def filter_by_state(data: List[Dict[str, Any]], state: str = "EXECUTED") -> List[Dict[str, Any]]:
     """Фильтрация словарей по значению ключа "state"""
     return [item for item in data if item.get("state") == state]
 
-def sort_by_date(data, reverse=True):
+
+def sort_by_date(data: List[Dict[str, Any]], reverse: bool = True) -> List[Dict[str, Any]]:
     """Сортирует список словарей по значению ключа "date"."""
     return sorted(data, key=lambda x: x["date"], reverse=reverse)
